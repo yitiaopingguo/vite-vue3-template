bfe654724f0199ef8dd56de2f91b4796b8b85e6e
lucf <2572274368@qq.com>
2023-11-23 16:31:13 +0800

    提交优化

--- src/views/home/components/homeContent.vue
+++ src/views/home/components/homeContent.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="content">
     <div class="page">
-
+      <div class="person">
+        你好，
+        <br/>我是一条苹果，
+        <br/>是一个前端开发工程师
+        <br/>我喜欢<span class="typed-text"></span>
+      </div>
+      <div>
+        <span id="fire1" class="fire"></span>
+        <span id="fire2" class="fire"></span>
+        <span id="fire3" class="fire"></span>
+      </div>
     </div>
-    <span class="typed-text"></span>
-    <span id="box"></span>
   </div>
 </template>
 
@@ -24,8 +32,10 @@ export default {
 
     },
     mounted() {
-      // this.initTyped()
-      this.initLottie()
+      this.initTyped()
+      this.initLottie('fire1')
+      this.initLottie('fire2')
+      this.initLottie('fire3')
     },
     computed: {
 
@@ -33,15 +43,15 @@ export default {
     methods: {
         initTyped() {
           new Typed('.typed-text', {
-            strings: ['Hello, World!', 'Welcome to Typed.js'],
+            strings: ['游泳', '滑滑板','打羽毛球'],
             typeSpeed: 50,
             backSpeed: 30,
             loop: true
           });
         },
-        initLottie(){
+        initLottie(id){
           lottie.loadAnimation({
-            container: document.getElementById('box'),
+            container: document.getElementById(id),
             renderer: 'svg',// 渲染方式：svg、canvas
             loop: true,  // 循环播放，默认：false
             autoplay: true, //自动播放 ，默认：true
@@ -54,13 +64,28 @@ export default {
 
 <style scoped lang="scss">
 .content {
-  height: calc(100vh - 80px);
   width: 100%;
   background-color: rgb(236, 237, 237);
+  margin-top: 90px;
   .page{
     width: 100%;
     height: 600px;
     background: url(../../../assets/images/page-hero-workspace.webp) center center no-repeat;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .person{
+    font-family: Heavy;
+    font-size: 30px;
+    text-align: center;
+    line-height: 50px;
+  }
+  
+  .fire{
+    display: inline-block;
+    width: 30px;
+    height: 30px;
   }
 }
 </style>

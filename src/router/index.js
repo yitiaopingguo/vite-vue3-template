6e9e58276c5d572989b609a65987c4e714da2c80
lucf <2572274368@qq.com>
2023-11-28 15:25:58 +0800

    代码更新1

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';//引入Vue对象，因为要用它的use
+import Router from 'vue-router'//引入vue-router
+//引入你需要配置的组件
+import MyHome from '../views/home/homeIndex.vue'
+//使用路由插件
+Vue.use(Router);
+
+//配置路由
+const router =  new Router({
+    routes: [
+        {
+            path:"/",
+            component:MyHome
+        },
+    ]
+});
+
+//导出
+export default router;
---
+++ src/views/home/components/homeFooter.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="footer">
+    <div class="footer-box">
+      <div class="use">
+        <div class="anim"></div>
+        <span>个人主页使用了：</span>
+        <el-tooltip class="box-item" effect="dark" content="Vue.js" placement="top">
+          <img class="iconfont" src="../../../assets/icon/Vue.png" alt="">
+        </el-tooltip>
+        <el-tooltip class="box-item" effect="dark" content="TypeScript" placement="top">
+          <img class="iconfont" src="../../../assets/icon/typescript.png" alt="">
+        </el-tooltip>
+        <el-tooltip class="box-item" effect="dark" content="ElementUI" placement="top">
+          <i class="el-icon-platform-eleme"></i>
+        </el-tooltip>
+        <el-tooltip class="box-item" effect="dark" content="Eslint" placement="top">
+          <img class="iconfont" src="../../../assets/icon/sass.png" alt="">
+        </el-tooltip>
+        <el-tooltip class="box-item" effect="dark" content="Sass" placement="top">
+          <img class="iconfont" src="../../../assets/icon/eslint.png" alt="">
+        </el-tooltip>
+        <div>豫ICP备12138号</div>
+      </div>
+      <div class="footer-right">
+        <img class="iconfont" src="../../../assets/icon/pingguo.png" alt="">
+        <img class="iconfont" src="../../../assets/icon/qq.png" alt="">
+        <img class="iconfont" src="../../../assets/icon/github.png" alt="">
+        <img class="iconfont" src="../../../assets/icon/zhihu.png" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.footer {
+  background-color: #f4f4f4;
+  height: 240px;
+  width: calc(100% - 100px);
+  margin: 20px 50px;
+  border-radius: 20px;
+  padding: 20px;
+  box-sizing: border-box;
+  .footer-box {
+    display: flex;
+    justify-content: space-between;
+  }
+  .anim {
+    height: 50px;
+    width: 50px;
+    background-color: #fff;
+  }
+  .use {
+    line-height: 24px;
+    span {
+      line-height: 24px;
+    }
+    .iconfont {
+      display: inline-block;
+      width: 16px;
+      height: 16px;
+      margin: 0 10px;
+    }
+  }
+
+  .icon-plus {
+    color: #4b32c3;
+    font-size: 18px;
+    width: 18px;
+    height: 18px;
+    margin: 0 10px;
+    line-height: 18px;
+    margin-bottom: -1px;
+  }
+  .footer-right {
+    padding-top: 160px;
+    .iconfont {
+      display: inline-block;
+      width: 16px;
+      height: 16px;
+      padding: 0 10px;
+    }
+  }
+}
+</style>
